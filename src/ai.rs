6bd7bd66c88933a4feeefadd7b24bde08ba84d9b
Jantony Velazquez Gauthier <jantony@localhost.localdomain>
2020-10-13 13:07:36 -0400

    Refactor minmax()

--- src/ai.rs
+++ src/ai.rs
@@ -3,16 +3,17 @@ use crate::game::{player, board};
 // returns (index, score) pair, the index corresponding to the index of the best move
 // and the score corresponding to the score that move got.
 pub fn minmax(player: player::Player, board: &board::Board) -> (Option<usize>, i8) {
+    let tile_score = |x| {
+        let mut b = board.clone();
+        b[x] = Some(player);
+        -minmax(!player, &b).1
+    };
 
     match board::game_state(player, board) {
         Some(score) => (None, score),
         None => (0..9)
             .filter(|&x| board[x].is_none())
-            .map(|x| {
-                let mut b = board.clone();
-                b[x] = Some(player);
-                (Some(x), -minmax(!player, &b).1)
-            })
+            .map(|x| (Some(x), tile_score(x)))
             .max_by_key(|x| x.1)
             .unwrap_or((None, 0)),
     }
